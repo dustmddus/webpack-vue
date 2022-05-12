b6b99b73c88c5db930b49efc5522eb89d3b384bf
Seung Yeon <82739503+dustmddus@users.noreply.github.com>
2022-05-12 19:28:09 +0900

    create project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <!-- 굳이 닫힘태그가 없어도 된다.  -->
+  <Hello />
+</template>
+
+<script>
+// 지역등록 해줌
+//
+import Hello from "~/components/Hello";
+
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: "Hello Webpack??..",
+    };
+  },
+};
+</script>
+
+<!-- <style>
+    h1 {
+    color: orange;
+    }
+</style> -->
